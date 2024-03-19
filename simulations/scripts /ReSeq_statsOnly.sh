9ecbee99adafb156a1858fd3eeee10a4b69a8575
AEWesdorp <60964589+AEWesdorp@users.noreply.github.com>
2024-03-19 11:24:29 +0100

    Create ReSeq_statsOnly.sh

---
+++ simulations/scripts /ReSeq_statsOnly.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+echo 'Human genome: ' $1 
+
+HumanGenome=$1
+
+###read generation using ReSeq 
+while read -r line
+do 
+	echo "$line"
+	file_dir=$(echo $line | awk '{print $1}')
+	echo $file_dir
+	name=$(echo $line | awk '{print $2}')
+	echo $name
+	
+	### GET RESEQ STATS ###
+	mkdir -p ../ReSeq_statsOnly/
+	mkdir -p ../ReSeq_statsOnly/${name}/
+
+	#combine sequencing data (lanes)
+	pigz -cd -p 32 ${file_dir}*R1*.fastq.gz > ../ReSeq_statsOnly/${name}/${name}_R1.fastq
+	pigz -cd -p 32 ${file_dir}*R2*.fastq.gz > ../ReSeq_statsOnly/${name}/${name}_R2.fastq
+
+	#mapping human reads 
+	bowtie2 -p 32 -X 2000 -x $HumanGenome \
+	-1 ../ReSeq_statsOnly/${name}/${name}_R1.fastq \
+	-2 ../ReSeq_statsOnly/${name}/${name}_R2.fastq \
+	| samtools sort -m 10G -@ 4 -T ../ReSeq_statsOnly/${name}/${name}.tmp \
+	-o ../ReSeq_statsOnly/${name}/${name}.bam -
+
+	#cleanup 
+	#rm ../ReSeq_statsOnly/${name}/${name}_R1.fastq
+	#rm ../ReSeq_statsOnly/${name}/${name}_R2.fastq
+
+	#get stats ReSeq
+	reseq illuminaPE -j 32 -r "${HumanGenome}.fna" \
+	-b ../ReSeq_statsOnly/${name}/${name}.bam --statsOnly --noTiles
+	
+done < ./input_sequencing_files.txt
