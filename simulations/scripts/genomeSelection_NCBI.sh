dfaf65dc9b4823de5e3b60b2dd35f53c56f93945
AEWesdorp <60964589+AEWesdorp@users.noreply.github.com>
2024-03-20 11:40:14 +0100

    Update genomeSelection_NCBI.sh

--- simulations/scripts/genomeSelection_NCBI.sh
+++ simulations/scripts/genomeSelection_NCBI.sh
@@ -6,5 +6,4 @@ mkdir -p ../sim_data/
 wget ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt -O ../sim_data/assembly_summary_refseq.txt
 
 grep -e "Penicillium" -e "Aspergillus" -e "Komagataella phaffii" -e "Mucor" -e "Rhizopus" -e "Pneumocystis" -e "Fusarium" -e "Botrytis cinerea" ../sim_data/assembly_summary_refseq.txt | \
-grep -v "virus" | cut -f 8 | sort | \
-uniq > ../sim_data/assembly_summary_refseq_ofInterest.txt
+grep -v "virus" | cut -f 8 | sort | uniq > ../sim_data/assembly_summary_refseq_ofInterest.txt
