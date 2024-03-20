924412cceb51a98f05a3e3d486113b8d7d21b642
AEWesdorp <60964589+AEWesdorp@users.noreply.github.com>
2024-03-20 11:32:00 +0100

    Update genomeSelection_NCBI.sh

--- simulations/scripts/genomeSelection_NCBI.sh
+++ simulations/scripts/genomeSelection_NCBI.sh
@@ -1,15 +1,11 @@
 #!/bin/bash 
 ### SELECT FUNGAL NCBI GENOMES FOR SIMULATION ###
 
-mkdir -p ../sim_data/
-mkdir -p ../sim_data/genomes_NCBI/
+mkdir -p ./sim_data/
+mkdir -p ./sim_data/genomes_NCBI/
 
-wget ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt -O ../sim_data/assembly_summary_refseq.txt
+wget ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt -O ./sim_data/assembly_summary_refseq.txt
 
-grep -e "Penicillium" -e "Aspergillus" -e "Komagataella phaffii" -e "Mucor" -e "Rhizopus" -e "Pneumocystis" -e "Fusarium" -e "Botrytis cinerea" ../sim_data/assembly_summary_refseq.txt | \
+grep -e "Penicillium" -e "Aspergillus" -e "Komagataella phaffii" -e "Mucor" -e "Rhizopus" -e "Pneumocystis" -e "Fusarium" -e "Botrytis cinerea" ./sim_data/assembly_summary_refseq.txt | \
 grep -v "virus" | cut -f 8 | sort | \
-uniq > ../sim_data/assembly_summary_refseq_ofInterest.txt
-
-grep -e "Penicillium" -e "Aspergillus" -e "Komagataella phaffii" -e "Mucor" -e "Rhizopus" -e "Pneumocystis" -e "Fusarium" -e "Botrytis cinerea" ../sim_data/assembly_summary_refseq.txt | \
-grep -v "virus" | cut -f 7-8 | sort | \
-uniq | sed 's| ||g' | perl -ane 'print "$F[1]\t$F[0]\n"' > ../NCBI_genome2taxid.map
+uniq > ./sim_data/assembly_summary_refseq_ofInterest.txt
