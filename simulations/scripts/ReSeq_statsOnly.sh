cb5b39f1fd3351d0dbf5e6c07deb73a96cc99c65
AEWesdorp <60964589+AEWesdorp@users.noreply.github.com>
2024-03-20 11:29:14 +0100

    Update ReSeq_statsOnly.sh

--- simulations/scripts/ReSeq_statsOnly.sh
+++ simulations/scripts/ReSeq_statsOnly.sh
@@ -13,27 +13,27 @@ do
 	echo $name
 	
 	### GET RESEQ STATS ###
- 	mkdir -p ../sim_data/
-	mkdir -p ../sim_data/ReSeq_statsOnly/
-	mkdir -p ../sim_data/ReSeq_statsOnly/${name}/
+ 	mkdir -p ./sim_data/
+	mkdir -p ./sim_data/ReSeq_statsOnly/
+	mkdir -p ./sim_data/ReSeq_statsOnly/${name}/
 
 	#combine sequencing data (lanes)
-	pigz -cd -p 32 ${file_dir}*R1*.fastq.gz > ../sim_data/ReSeq_statsOnly/${name}/${name}_R1.fastq
-	pigz -cd -p 32 ${file_dir}*R2*.fastq.gz > ../sim_data/ReSeq_statsOnly/${name}/${name}_R2.fastq
+	pigz -cd -p 32 ${file_dir}*R1*.fastq.gz > ./sim_data/ReSeq_statsOnly/${name}/${name}_R1.fastq
+	pigz -cd -p 32 ${file_dir}*R2*.fastq.gz > ./sim_data/ReSeq_statsOnly/${name}/${name}_R2.fastq
 
 	#mapping human reads 
 	bowtie2 -p 32 -X 2000 -x $HumanGenome \
-	-1 ../sim_data/ReSeq_statsOnly/${name}/${name}_R1.fastq \
-	-2 ../sim_data/ReSeq_statsOnly/${name}/${name}_R2.fastq \
-	| samtools sort -m 10G -@ 4 -T ../sim_data/ReSeq_statsOnly/${name}/${name}.tmp \
-	-o ../sim_data/ReSeq_statsOnly/${name}/${name}.bam -
+	-1 ./sim_data/ReSeq_statsOnly/${name}/${name}_R1.fastq \
+	-2 ./sim_data/ReSeq_statsOnly/${name}/${name}_R2.fastq \
+	| samtools sort -m 10G -@ 4 -T ./sim_data/ReSeq_statsOnly/${name}/${name}.tmp \
+	-o ./sim_data/ReSeq_statsOnly/${name}/${name}.bam -
 
 	#cleanup 
-	#rm ../sim_data/ReSeq_statsOnly/${name}/${name}_R1.fastq
-	#rm ../sim_data/ReSeq_statsOnly/${name}/${name}_R2.fastq
+	#rm ./sim_data/ReSeq_statsOnly/${name}/${name}_R1.fastq
+	#rm ./sim_data/ReSeq_statsOnly/${name}/${name}_R2.fastq
 
 	#get stats ReSeq
 	reseq illuminaPE -j 32 -r "${HumanGenome}.fna" \
-	-b ../sim_data/ReSeq_statsOnly/${name}/${name}.bam --statsOnly --noTiles
+	-b ./sim_data/ReSeq_statsOnly/${name}/${name}.bam --statsOnly --noTiles
 	
 done < ./input_sequencing_files.txt
